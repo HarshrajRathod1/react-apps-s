ccaba269c7ebb57f7d04d844d94ab8bbc572cf4e
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-07-15 18:09:33 +0530

    object predefined methods

--- first-app/src/componets/DataBind.jsx
+++ first-app/src/componets/DataBind.jsx
@@ -8,6 +8,21 @@ export function DataBind(){
             <p>Name: {studData.name}</p>
             <p>Age: {studData.age}</p>
             <p>Course: {studData.course}</p>
+            <hr></hr>
+            <h2>Keys of object</h2>
+            {
+                Object.keys(studData).map((k,i)=>(<p key={i}>{k}</p>))
+            }
+            <hr/>
+            <h2>Values</h2>
+            {
+                Object.values(studData).map((v,i)=>(<p key={i}>{v}</p>))
+            }
+            <hr/>
+            <h2>Entries</h2>
+            {
+                Object.entries(studData).map(e=>(<p key={e}>{e}</p>))
+            }
         </div>
     )
 }
