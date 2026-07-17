8fce5980858367698ae646c95394d619770f78e7
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-07-17 23:04:29 +0530

    add header

--- shoping-app/src/components/shoping/Shoping.jsx
+++ shoping-app/src/components/shoping/Shoping.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export function Shoping(){
+    const [category,setCategory]=useState([])
+    const [product,setProduct]=useState({'products':[]})
+
+    function LoadCategory(){
+        axios.get('https://dummyjson.com/products/category-list')
+        .then((response)=>setCategory(response.data))
+    }
+    useEffect(()=>{
+        LoadCategory()
+    },[])
     return(
-        <div>
-            Hello World!
+        <div className="container-fluid">
+            <header className="d-flex flex-row justify-content-between align-items-center bg-light p-2">
+                <div className="bi bi-bag-fill fs-3 fw-bold"> Shopping</div>
+                <div>
+                    <div className="input-group"><input type="text" className="form-control" /><span className="bi bi-search btn btn-warning"></span></div>
+                </div>
+                <div>
+                    <span className="bi bi-person btn btn-secondary rounded mx-2"></span>
+                    <span className="bi bi-cart btn btn-warning position-realtive"><span className="badge bg-danger rounded rounded-circle position-absolute">0</span></span>
+                </div>
+            </header>
         </div>
     )
 }
--- shoping-app/src/main.jsx
+++ shoping-app/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.jsx'
 import {Shoping} from './components/shoping/Shoping.jsx'
 
