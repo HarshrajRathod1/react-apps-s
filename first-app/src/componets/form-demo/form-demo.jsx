e87f4bc82e1f3a68691dcdecf93d1ec7db5ef88b
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-07-22 16:01:01 +0530

    form handling using useRef hook in formdemo

--- first-app/src/componets/form-demo/form-demo.jsx
+++ first-app/src/componets/form-demo/form-demo.jsx
@@ -1,32 +1,35 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export function FormDemo(){
+    let userRef=useRef('')
+    let ageRef=useRef('')
+    let cityRef=useRef('')
+    let genderRef=useRef('')
     function handleForm(e){
         e.preventDefault()
-        let data=new FormData(e.target)
-        console.log({username:data.get("user"),age:data.get("age"),city:data.get("city"),gender:data.get("gender")})
+        console.log({username:userRef.current.value,age:ageRef.current.value,city:cityRef.current.value,gender:genderRef.current.value})
     }
     return (
         <div className="container-fluid">
             <h3>User Registration</h3>
-            <form onSubmit={handleForm} method="post">
+            <form onSubmit={handleForm}>
                 <dl>
                     <dt>User Name</dt>
-                    <dd><input type="text" name="user"/></dd>
+                    <dd><input type="text" name="user" ref={userRef}/></dd>
                     <dt>Age</dt>
-                    <dd><input type="number" name="age"/></dd>
+                    <dd><input type="number" name="age" ref={ageRef}/></dd>
                     <dt>City</dt>
                     <dd>
-                        <select name="city">
+                        <select name="city" ref={cityRef}>
                             <option>Delhi</option>
                             <option>Hyd</option>
                         </select>
                     </dd>
                     <dt>Gender</dt>
                     <dd>
-                        <input type="radio" name="gender" value="male"/>
+                        <input type="radio" name="gender" value="male" ref={genderRef}/>
                         Male
-                        <input type="radio" name="gender" value="female"/>
+                        <input type="radio" name="gender" value="female" ref={genderRef}/>
                         Female
                     </dd>
                 </dl>
