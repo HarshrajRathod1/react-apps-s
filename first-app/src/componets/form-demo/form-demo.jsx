adca674a6478d23ae1a28dca668b3fc2cadbfa07
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-07-23 13:03:19 +0530

    form with custom validation, handle blur with 3rd party module formik

--- first-app/src/componets/form-demo/form-demo.jsx
+++ first-app/src/componets/form-demo/form-demo.jsx
@@ -1,39 +1,90 @@
 import { useRef, useState } from "react"
-
+import { useFormik } from "formik"
 export function FormDemo(){
-    let userRef=useRef('')
-    let ageRef=useRef('')
-    let cityRef=useRef('')
-    let genderRef=useRef('')
-    function handleForm(e){
-        e.preventDefault()
-        console.log({username:userRef.current.value,age:ageRef.current.value,city:cityRef.current.value,gender:genderRef.current.value})
-    }
+    function validateUser(dataForm){
+            let errors={}
+            if(dataForm.User.length===0){
+                errors.User="Username Required"
+            }else{
+                if(dataForm.User.length<4){
+                    errors.User="Name too short"
+                }
+            }
+
+            if(dataForm.Age.length===0){
+                errors.Age="Age required"
+            }else{
+                if(isNaN(dataForm.Age)){
+                    errors.Age="Age must be number"
+                }
+            }
+
+            if(dataForm.City=="-1"){
+                errors.City="Select your city"
+            }
+
+            if(dataForm.Gender.length===0){
+                errors.Gender="select your gender"
+            }
+            if (dataForm.Mobile.length===10){
+                errors.Mobile="mobile required"
+            }else{
+                if(!dataForm.Mobile.match(/^\+91\d{10}$/)){
+                    errors.Mobile="Invalid Mobile"
+                }
+            }
+            return errors
+        }
+    const formik=useFormik({
+        initialValues:{
+            User:'',
+            Age:'',
+            City:'-1',
+            Gender:'',
+            Mobile:''
+        },
+        validate:validateUser,
+        onSubmit:function (user){
+            console.log(user)
+        },
+    })
     return (
         <div className="container-fluid">
             <h3>User Registration</h3>
-            <form onSubmit={handleForm}>
+            <form onSubmit={formik.handleSubmit}>
                 <dl>
                     <dt>User Name</dt>
-                    <dd><input type="text" name="user" ref={userRef}/></dd>
+                    <dd><input type="text" name="User" onBlur={formik.handleBlur} onChange={formik.handleChange}/></dd>
+                    <dd className="text-danger">{formik.touched.User && formik.errors.User}</dd>
+
                     <dt>Age</dt>
-                    <dd><input type="number" name="age" ref={ageRef}/></dd>
+                    <dd><input type="text" name="Age" onBlur={formik.handleBlur} onChange={formik.handleChange}/></dd>
+                    <dd className="text-danger">{formik.touched.Age && formik.errors.Age}</dd>
+                    
                     <dt>City</dt>
                     <dd>
-                        <select name="city" ref={cityRef}>
-                            <option>Delhi</option>
-                            <option>Hyd</option>
+                        <select name="City" onBlur={formik.handleBlur} onChange={formik.handleChange}>
+                            <option value="-1">None</option>
+                            <option value="hyd">Hyd</option>
+                            <option value="delhi">Delhi</option>
                         </select>
                     </dd>
+                    <dd className="text-danger">{formik.touched.City &&formik.errors.City}</dd>
+
                     <dt>Gender</dt>
-                    <dd>
-                        <input type="radio" name="gender" value="male" ref={genderRef}/>
-                        Male
-                        <input type="radio" name="gender" value="female" ref={genderRef}/>
-                        Female
+                    <dd><input type="radio" value="male" name="Gender" onBlur={formik.handleBlur} onChange={formik.handleChange}/> Male <span> &nbsp; </span>
+                        <input type="radio" value="female" name="Gender" onBlur={formik.handleBlur} onChange={formik.handleChange}/> Female
                     </dd>
+                    <dd className="text-danger">{formik.touched.Gender && formik.errors.Gender}</dd>
+
+                    <dt>Mobile</dt>
+                    <dd>
+                        <input type="text" onBlur={formik.handleBlur} onChange={formik.handleChange} name="Mobile" />
+                    </dd>
+                    <dd className="text-danger">{formik.touched.Mobile && formik.errors.Mobile}</dd>
                 </dl>
-                <button>Register</button>
+
+                <button type="submit">Register</button>
             </form>
         </div>
     )
