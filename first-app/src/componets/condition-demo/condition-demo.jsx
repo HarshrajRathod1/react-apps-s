0fe8f10b179a24e4dd140d54d32c4cf7ad97bd93
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-07-31 14:44:47 +0530

    conditiondemo.jsx inside render a specific container into component using condition , also used sessionStorage

--- first-app/src/componets/condition-demo/condition-demo.jsx
+++ first-app/src/componets/condition-demo/condition-demo.jsx
@@ -1,30 +1,45 @@
 import { useState } from "react"
-import { Login } from "../login/login"
-import { FakestoreIndex } from "../fakestore/fakestore-index"
-import {FormikDemo} from "../formik-demo/formik-demo"
-
 export function ConditionDemo(){
 
-    const [display,setDisplay]=useState('')
+    const [name,setname]=useState('')
 
-    function handleClick(e){
-        switch(e.target.name){
-            case 'login':
-                setDisplay(<FormikDemo/>)
-                break;
-            case 'shopping':
-                setDisplay(<FakestoreIndex  />)
-                break;
-        }
+    function handlename(e){
+        setname(e.target.value)
+    }
+
+    function handleSignIn(){
+        sessionStorage.setItem("name",name)
+        location.reload()
+        
+    }
+    function handleSignOut(){
+        sessionStorage.removeItem("name")
+        location.reload()
     }
     return(
-        <div>
-            <h2>Condition-Demo</h2>
-            <button onClick={handleClick} name="login">Login</button>
-            &nbsp;&nbsp;
-            <button onClick={handleClick} name="shopping">Shopping</button>
-            <hr></hr>
-            {display}
+        <div className="container-fluid">
+            <header className="d-flex justify-content-between align-items-center p-2 bg-light">
+                <span className="bi bi-amazon fs-4 fw-bold"> Amazon</span>
+                <div>
+                    <div className="input-group">
+                    <input placeholder="eg: brands, products" type="text" className="form-control"></input>
+                    <button className="bi bi-search btn btn-warning"></button>
+                    </div>
+                </div>
+                <div>
+                    {(sessionStorage.getItem("name")===null)?
+                    <div className="input-group">
+                        <input onChange={handlename} placeholder="eg: username" type="text" name="uname" className="form-control"/>
+                        <button onClick={handleSignIn} className="btn btn-danger">SignIn</button>
+                    </div>
+                    :
+                    <div>
+                        <span>{sessionStorage.getItem("name")}</span>
+                        <button onClick={handleSignOut} className="btn btn-danger mx-3">SignOut</button>
+                    </div>
+                    }
+                </div>
+            </header>
         </div>
     )
 }
