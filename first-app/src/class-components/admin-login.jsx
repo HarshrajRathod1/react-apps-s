b49189d8df4bbb934c4faf9382f26fc000b86c32
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-08-21 12:29:24 +0530

    class inside state, effect

--- first-app/src/class-components/admin-login.jsx
+++ first-app/src/class-components/admin-login.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+
 export class AdminLogin extends React.Component{
     constructor(){
         super();
+        this.state={title:"This is a State",category:['mobile','laptop','charger']}
     }
+    componentDidMount(){
+        this.setState({title:"Shopping App"})
+    }
+
     render(){
         return(
-            <div className="container-fluid">
-                <h3 className="text-center ">Admin Pannel</h3>
-            </div>
+            <div className="container-fluid text-center">
+                <h3>{this.state.title}</h3>
+                <ul>
+                    {
+                        this.state.category.map(item=>(
+                            <li key={item}>{item}</li>
+                        ))
+                    }
+                </ul>
+            </div>  
         )
     }
 }
