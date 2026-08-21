e7bb7f3883a9164de12477dc43e9a34dde0b6b57
HarshrajRathod1 <harshrajrathod91@gmail.com>
2026-08-21 12:50:29 +0530

    event in class components

--- first-app/src/class-components/admin-login.jsx
+++ first-app/src/class-components/admin-login.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+ import React from "react";
 
-export class AdminLogin extends React.Component{
+ export class AdminLogin extends React.Component{
     constructor(){
-        super();
-        this.state={title:"This is a State",category:['mobile','laptop','charger']}
+        super()
+        this.state={
+            msg:'',
+            count:0,
+        }
+        this.handleInsertClick=this.handleInsertClick.bind(this)
+        this.handleUpdateClick=this.handleUpdateClick.bind(this)
+        this.handleCountIncrease=this.handleCountIncrease.bind(this)
+        this.handleCountDecrease=this.handleCountDecrease.bind(this)
     }
-    componentDidMount(){
-        this.setState({title:"Shopping App"})
+    handleInsertClick(){
+        this.setState({msg:"Recored Inserted Successfully"})
+    }
+    handleUpdateClick(){
+        this.setState({msg:"Recored Updated"})
+    }
+
+    handleCountIncrease(){
+        this.setState({count:this.state.count+1})
+    }
+    handleCountDecrease(){
+        this.setState({count:this.state.count-1})
     }
 
     render(){
         return(
-            <div className="container-fluid text-center">
-                <h3>{this.state.title}</h3>
-                <ul>
-                    {
-                        this.state.category.map(item=>(
-                            <li key={item}>{item}</li>
-                        ))
-                    }
-                </ul>
-            </div>  
+            <div className="container-fluid m-2">
+                <button onClick={this.handleInsertClick}>Insert</button>
+                <button onClick={this.handleUpdateClick} className="mx-2">Update</button>
+                <p>{this.state.msg}</p>
+                <hr/>
+                <p>Count:{this.state.count}</p>
+                <button onClick={this.handleCountIncrease}>count : +1</button>
+                <button onClick={this.handleCountDecrease} className="mx-3">count : -1</button>
+            </div>
         )
     }
-}
+ }
